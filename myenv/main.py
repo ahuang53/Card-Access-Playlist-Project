56121f4a7f91d1b64ed9a10fbfa6b90bb4410c09
Andy <huanga9@rpi.edu>
2024-06-24 17:24:25 -0400

    Remove spotipy search function and replaced with Genius API searching

--- myenv/main.py
+++ myenv/main.py
@@ -2,44 +2,49 @@
 This is the main file for this project.
 
 """
-
-import spotipy #Python library for using spotify data 
-from spotipy.oauth2 import SpotifyClientCredentials #Authenticating API
-
 import os #Accessing environment variables
 from dotenv import load_dotenv
 
-load_dotenv() #Load the env file
+import lyricsgenius as lg
 
+load_dotenv() #Load the env file
 def main():
 
     #Authentication
-    auth_manager = SpotifyClientCredentials(client_id =os.getenv('client_id'), client_secret = os.getenv('client_secret'))
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+    genius = lg.Genius(os.getenv('GENIUS_ACCESS_TOKEN')) #Genius API Obj 
 
-    #Data Structures
-    results = dict() #Stores search results
+    #Search for Artist & Track
+    while(1):
+        print("Enter the song name: ") 
+        track = input() 
+        print("\nEnter the artist's name(leave blank if unknown): ")
+        art = input()
+        
+        if (track != "" and art != ""): #both track and artist are inputted
+            query = genius.search_song(track,art) #search for song
+            if((query!= None)): #Error Check
+                print("\n\n" + query.full_title)
+            else:
+                print("\nERROR: Track was not found") #Error
+        elif (track != ""): #only track is inputted
+            query = genius.search_song(track)
+            if((query != None)): #Error Check
+                print("\n\n" + query.full_title)
+            else:
+                print("\nERROR: Track was not found")
+        elif (art != ""): #only artist is inputted
+            print("\nERROR: Track was not found")
+        else: #nothing is inputted 
+            print("\nERROR: Track was not found")
+        
+        #Confirmation screen
+        print("\nIs this the correct song? Yes or no") 
+        confirm = input()
+        if(confirm.strip().lower() == "yes"):
+            break
 
-    #Search for Artist
-    print("Enter the song name: ") 
-    track_q = input() #inputs
-    #print("Enter the artist's name(leave blank if unknown): ")
-    #art = input()
+    print("Hello")
 
-    #print("\nThis is {} by {}".format(track,art))
-    name = sp.search(track_q, 3, 0,'track') #Outputs a Dictonary Object 
-    items_list = name['tracks']['items'] #track details as list obj 
-    print(len(name['tracks']['items']))
     
-    for i in range(0,len((items_list))): #Runs through top 3 results
-        #print("-------------------\n")
-        results[items_list[i]['album']["name"]] =( #Name of song searched
-        items_list[i]['artists'][0]['name'] ) #Name of artist
-        #print((items_list[i]['album']["name"])) #Name of song searched
-        #print(" [Album obj] \n")
-        #print("-_-_-_-_-_-_-_-_\n")
-        #print(items_list[i]['artists'][0]['name']) #name of artist
-    print(results)
-
 if __name__ == "__main__":
     main()
