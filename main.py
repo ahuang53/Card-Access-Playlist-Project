e0a5a5532cc958099a911ecc6092336c2d2c4084
AndyH <andyhu35@gmail.com>
2024-07-24 15:18:50 -0400

    added operation mode and added music player stop and pause functions

--- main.py
+++ main.py
@@ -39,18 +39,24 @@ def main():
     #Authentication
     local_playlist = [] #Stores all the local songs being played
     genius = lg.Genius(os.getenv('GENIUS_ACCESS_TOKEN')) #Genius API Obj 
-    found = sg.track_select(genius) #Found is the correct song
-    print(store_song(found))
-    '''
+    
     #Operation Mode
-    print("Please indicate the Operation Mode(I for Introduction, P for playlist mode, S for searching):")
-    op_mode = input()
-    if(op_mode.strip().upper() == "S"):
-        #Song searching
-        found = track_select(genius) #Found is the correct song
-    '''
-    #vlc_play(hash_string(found.full_title))
-    
-    
+    while(1):
+        print("Please indicate the Operation Mode(Intro for Introduction, Play for playlist mode, Search for searching):")
+        op_mode = input()
+        if(op_mode.strip().lower() == 'search'): #Search for sng mode
+            #Song searching
+            found = sg.track_select(genius) #Found is the correct song
+            local_playlist.append(store_song(found)) #Add song to local playlsit
+        elif(op_mode.strip().lower() == "play"):
+            if(len(local_playlist) == 0):
+                print('Error: Playlist is empty. Please add songs to the playlist before proceeding\n')
+                continue
+            for i in range(len(local_playlist)): #Runs through all songs in local playlist
+                mp.vlc_play(local_playlist[i].get_ID())
+        #elif(op_mode.strip().upper() == "INTRO"): intromode needs student ids and an assigned song 
+        elif(op_mode.strip().lower() == "exit"):
+            break
+
 if __name__ == "__main__":
     main()
--- music_playing.py
+++ music_playing.py
@@ -16,7 +16,6 @@ def search_file(directory,file_name):
             return os.path.join(directory, filename)
     return None
 
-
 '''
 This function belows run the VLC media player to play a specific file.
 It takes in the hash id of the file name
@@ -24,6 +23,9 @@ It takes in the hash id of the file name
 def vlc_play(hash_id):
     vlc_path = r'C:\Program Files\VideoLAN\VLC\vlc.exe'
     file_path = search_file(os.getcwd(),hash_id)
+
+    #Stop flag for ending the playback
+    stop_flag = False
 
     # Create a VLC instance with the specified path
     instance = vlc.Instance()
@@ -35,11 +37,16 @@ def vlc_play(hash_id):
 
     # Start playing the stream
     media_player.play()
-    sleep(5)
+    print("Playing track...")
 
-    if(media_player.is_playing()):
-        print("Playing song...")
-    while media_player.is_playing():
-        sleep(1)
-    if(media_player.is_playing() != 1):
-        print("Song has ended...")
+    #Stop and pause function
+    while(stop_flag != True):
+        playback = input()
+        if(playback.strip().lower() == 's'):
+            media_player.stop()
+            stop_flag = True
+            print("Track has stopped...")
+        elif(playback.strip().lower() == 'p'):
+            media_player.pause()
+            print("Track has been paused...")
+        
